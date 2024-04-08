c25c584cd9fa88eea365da923ec243927838a3cc
DieuMy25 <131366686+DieuMy25@users.noreply.github.com>
2024-04-09 03:57:25 +0700

    my

---
+++ SweetCake/src/main/java/com/sweetcake/servlet/CartServlet.java
@@ -0,0 +1,27 @@
+package com.sweetcake.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CartServlet
+ */
+@WebServlet("/sweetcake/cart")
+public class CartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.getRequestDispatcher("/views/user/cart.jsp").forward(req, resp);
+		
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+}
